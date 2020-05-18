9a85ac62263a138da62ef1d03535b62f05c603e4
Didas Mbalanya <didasmbalanya@gmail.com>
2020-05-18 02:25:52 +0200

    Updates

---
+++ precache-manifest.2c188b122d73ddeb0ffbaa201acfaf1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bee26237814d790f268c3657a59161",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "e73cc71e199fb9a529dc",
+    "url": "/image-search/static/css/main.be7d16f6.chunk.css"
+  },
+  {
+    "revision": "112be4201bb7173b7bc2",
+    "url": "/image-search/static/js/2.a464eb6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-search/static/js/2.a464eb6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73cc71e199fb9a529dc",
+    "url": "/image-search/static/js/main.f59782c6.chunk.js"
+  },
+  {
+    "revision": "8a294dda0feab5349d0d",
+    "url": "/image-search/static/js/runtime-main.8298d98b.js"
+  }
+]);
